8dde552766265f945dbf2655edef7f31206b5212
kinyoubi0b <kinyoubi0b@gmail.com>
2022-10-08 14:09:36 +0800

    finish ch7

---
+++ ch7/restaurant/src/lib.rs
@@ -0,0 +1,43 @@
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
+    }
+
+    fn cook_order() {}
+}
+
+fn serve_order() {}
+
+mod front_of_house;
+use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
