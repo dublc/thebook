11a055d60dab240d96d43a788fc3964f0d11f8fd
kinyoubi0b <kinyoubi0b@gmail.com>
2022-10-24 01:57:08 +0800

    finish ch14

---
+++ ch14/more-about-crate/src/lib.rs
@@ -0,0 +1,18 @@
+//! # More About Crate
+//!
+//! `more_about_crate` is a collection of utilities to make performing certain
+//! calculations more convenient.
+
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = more_about_crate::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
